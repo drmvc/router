21f8062bf985a11c5e50994c11d66bd2903eb4d7
Paul Rock <paul@drteam.rocks>
2018-03-29 20:25:30 +0300

    example fix

--- extra/route.php
+++ extra/route.php
@@ -2,12 +2,11 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use DrMVC\Router\Route;
-use DrMVC\Router\Error;
 
 $route = new Route('get', '/<action>', function () {
     echo 'asd';
 });
 print_r($route);
 
-$route = new Route('get', '/<zzz>', Error::class . ':test');
+$route = new Route('get', '/<zzz>', \DrMVC\Router\Error::class . ':test');
 print_r($route);
